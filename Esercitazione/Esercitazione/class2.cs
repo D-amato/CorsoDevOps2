b48c6488f5ce897f54581a071dd8128ebb078b2e
Davide Amato <davide460@gmail.com>
2018-06-20 16:30:30 +0200

    Aggiunte proprietà class2

--- Esercitazione/Esercitazione/class2.cs
+++ Esercitazione/Esercitazione/class2.cs
@@ -10,5 +10,7 @@ namespace Esercitazione
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
     }
 }
